104a0d5c35da6f223a11eedf50cf244c11d1cc33
Davide Rinarelli <rinarellidavide@gmail.com>
2026-06-14 23:00:35 +0200

    -
    Signed-off-by: Davide Rinarelli <rinarellidavide@gmail.com>

--- module.nix
+++ module.nix
@@ -161,8 +161,9 @@
             cat <<EOF > $out/bin/Hyprland
             #!/usr/bin/env bash
             
-            # Se è start-hyprland a chiamare per chiedere la versione o le info di sistema, bypassiamo il config
-            if [[ " \$@ " =~ " --version " ]] || [[ " \$@ " =~ " -i " ]]; then
+            echo "Hyprland called with args: \$@" >> /tmp/hypr-args.txt
+            
+            if [[ " \$@ " =~ " --version " ]] || [[ " \$@ " =~ " -i " ]] || [[ " \$@ " =~ " --systeminfo " ]] || [[ " \$@ " =~ " --help " ]]; then
                 exec -a "\$0" "$out/bin/.Hyprland-wrapped" "\$@"
             else
                 exec -a "\$0" "$out/bin/.Hyprland-wrapped" -c "${entrypointLua}" "\$@"
