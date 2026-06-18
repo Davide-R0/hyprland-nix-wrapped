28b1bc786a879e5723b29a130c2e75b96a87e167
Davide Rinarelli <rinarellidavide@gmail.com>
2026-06-18 22:52:17 +0200

    chore
    Signed-off-by: Davide Rinarelli <rinarellidavide@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.apply(nixInfo)
     -- Avvio automatico (Exec-once)
-    hl.on("hyprland.start", function()
+    hl.on('hyprland.start', function()
         --hl.exec_cmd(terminal)
         --hl.exec_cmd("nm-applet")
         --hl.exec_cmd("waybar & hyprpaper & firefox") -- Execute waybar, hyprpaper, firefox
